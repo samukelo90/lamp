ff40abf41c30b5ce892bebd231f0e28c63c5f167
samukelo90 <138897873+samukelo90@users.noreply.github.com>
2023-07-10 02:12:00 +0200

    Add files via upload

---
+++ click.js
@@ -0,0 +1,8 @@
+// var btn = document.getElementById("btn");
+// var btn = document.getElementById("btn");
+
+// function toggleBtn(){
+//   btn.classList.toggle("active");
+//   light.classList.toggle("on")
+// }
+
